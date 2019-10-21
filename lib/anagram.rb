725365c3ab84f678cd99ea16d8098dd1016709ed
Stefanie Davis <davisstefanie2010@gmail.com>
2019-10-21 06:27:17 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+class Anagram
+  
+  attr_accessor :word
+  
+  def initialize(word) 
+      @word = word 
+  end 
+  
+  def match(array)
+      array.select do  |string|
+      string.chars.sort == @word.chars.sort
+      end
+  end 
+  
+end 
